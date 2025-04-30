3954ceba431b0c128d39795672ddbc7cc4568b6b
Ricky Clautaire SAKA KEUGEU <sakaricky91@gmail.com>
2025-04-30 15:12:46 -0600

    chore: add logger

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
+
 from flask import Flask;
-from config import Config
+from config import Config, logger
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 
 db = SQLAlchemy()
 
@@ -10,7 +13,18 @@ def create_app():
 
     db.init_app(app)
 
+    # Use app.app_context() to ensure engine is accessible if needed,
+    with app.app_context():
+
+        @event.listens_for(Engine, "connect")
+        def receive_connect(dbapi_connection, connection_record):
+            """Listen for new DB connections and log them."""
+            # Use the Flask app's logger for consistency
+            logger.info("Successfully established a new DB connection.")
+
     from . import routes
     app.register_blueprint(routes.bp)
+
+    app.logger.info("Your Flask app have started")
 
     return app
--- config.py
+++ config.py
@@ -1,7 +1,28 @@
 import os
 from dotenv import load_dotenv
+import logging
+import colorlog
 
 load_dotenv()
+
+# Set up colorized logging
+logger = logging.getLogger()
+logHandler = colorlog.StreamHandler()
+
+formatter = colorlog.ColoredFormatter(
+    log_colors={
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'bold_red',
+    }
+)
+logHandler.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(logHandler)
+logger.setLevel(logging.INFO)
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
